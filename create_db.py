03a4df03ce821c02ad86c0f395798c6e50db5a31
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2019-02-05 17:36:47 -0500

    created script to generate models

---
+++ create_db.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = r"sqlite:///soen487a1.sqlite"
+db = SQLAlchemy(app)
+
+## Copy Models from models.py
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.Text(), nullable=False, unique=True)
+    createdConvo = db.relationship("Conversation", backref="creator", lazy="dynamic", foreign_keys="Conversation.creator_id")
+    participantConvo = db.relationship("Conversation", backref="participant", lazy="dynamic" ,foreign_keys="Conversation.participant_id")
+    messages_sent = db.relationship("Message")
+    # def __repr__(self):
+    #     return "<User {}: {}>".format(self.id, self.username)
+
+
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    participant_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    messages = db.relationship("Message", backref="messages")
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    conversation_id = db.Column(db.Integer, db.ForeignKey('conversation.id'))
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # creation_time = db.Column(DateTime)
+    text = db.Column(db.Text(), nullable=False)
